8e05bf42b2385c11a23be4da2efc07e95578e002
doyalikess <zachsaliba123@gmail.com>
2025-11-29 00:38:40 +0100

    Update promoCode.js

--- models/promoCode.js
+++ models/promoCode.js
@@ -1,36 +1,31 @@
 const mongoose = require('mongoose');
 
-const tipSchema = new mongoose.Schema({
-  fromUser: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-    required: true
-  },
-  toUser: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-    required: true
-  },
-  amount: {
-    type: Number,
-    required: true,
-    min: 0
-  },
-  unwageredAmount: {
-    type: Number,
-    default: function() {
-      return this.amount;
-    }
-  },
-  wageringProgress: {
-    totalWageredSinceTip: {
-      type: Number,
-      default: 0
-    }
-  },
-  message: {
+const promoSchema = new mongoose.Schema({
+  code: {
     type: String,
-    default: ''
+    required: true,
+    unique: true,
+    uppercase: true
+  },
+  value: {
+    type: Number,
+    required: true
+  },
+  maxUses: {
+    type: Number,
+    default: 1
+  },
+  uses: {
+    type: Number,
+    default: 0
+  },
+  createdBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  expiresAt: {
+    type: Date
   },
   createdAt: {
     type: Date,
@@ -38,17 +33,4 @@ const tipSchema = new mongoose.Schema({
   }
 });
 
-// Index for faster queries
-tipSchema.index({ fromUser: 1, createdAt: -1 });
-tipSchema.index({ toUser: 1, createdAt: -1 });
-tipSchema.index({ createdAt: -1 });
-
-// Virtual for remaining wagering requirement
-tipSchema.virtual('remainingWagering').get(function() {
-  return Math.max(0, this.unwageredAmount - this.wageringProgress.totalWageredSinceTip);
-});
-
-tipSchema.set('toJSON', { virtuals: true });
-tipSchema.set('toObject', { virtuals: true });
-
-module.exports = mongoose.model('Tip', tipSchema);
+module.exports = mongoose.model('PromoCode', promoSchema);
